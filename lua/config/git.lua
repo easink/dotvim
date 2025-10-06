a017e3ea1a48517200ea842d290ad1c2678f0fa0
Andreas Rydbrink <andreas.rydbrink@gmail.com>
2025-10-06 18:56:08 +0200

    vim-fugitive -> neogit

--- lua/config/git.lua
+++ lua/config/git.lua
@@ -1,14 +1,27 @@
 return {
-   -- git
-   {
-       'tpope/vim-fugitive',
-       init = function()
-         vim.opt.statusline = '[%n] %t %F %m%r%h%w%{fugitive#statusline()}%=%y [%{&ff}] %l,%v %p%%'
+    -- git
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
 
-       end,
+            -- Only one of these is needed.
+            "nvim-telescope/telescope.nvim",
+            -- "ibhagwan/fzf-lua",
+            -- "echasnovski/mini.pick",
+            -- "folke/snacks.nvim",
+        },
     }
-   -- 'gregsexton/gitv',
-   -- 'tpope/vim-git',
-   -- 'http://www.tidraso.co.uk/repository/vim-plugin/',
+    -- {
+    --     'tpope/vim-fugitive',
+    --     init = function()
+    --       vim.opt.statusline = '[%n] %t %F %m%r%h%w%{fugitive#statusline()}%=%y [%{&ff}] %l,%v %p%%'
+
+    --     end,
+    --  }
+    -- 'gregsexton/gitv',
+    -- 'tpope/vim-git',
+    -- 'http://www.tidraso.co.uk/repository/vim-plugin/',
 
 }
